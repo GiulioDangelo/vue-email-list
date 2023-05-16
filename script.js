83f3fd475986d300c63dc8cda66cac81988e2d8f
GiulioDangelo <dangelogiulio48@gmail.com>
2023-05-16 16:27:28 +0200

    fix loop

--- script.js
+++ script.js
@@ -1,35 +1,24 @@
 const app = Vue.createApp({
-    data() {
-        return {
-            mails : [],
-        }
-    },
+	data() {
+		return {
+			mails: [],
+		};
+	},
 
-    methods: {
-        randMail() {
-            // for (let i = 0; i < 10; i++) {
-                axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
-                .then((request) => this.mails = request.data.response)
-            // }
-        }
-
+	methods: {
+		randMail() {
+			axios.get("https://flynn.boolean.careers/exercises/api/random/mail")
+            .then((request) => this.mails.push(request.data.response));
+		},
+	},
 
 
-        // methods: {
-        //     randMail() {
-        //         axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
-        //         .then((request) => { this.mails.push(request.data.response)})
-        // },
-    },
 
-     created() {
-        for (let i = 0; i < 10; i++) {
-           this.randMail()
-        }
+	created() {
+		for (let i = 0; i < 10; i++) {
+			this.randMail();
+		}
+	},
+});
 
-
-    // this.randMail()
-     },
-})
-
-app.mount('#app')
+app.mount("#app");
